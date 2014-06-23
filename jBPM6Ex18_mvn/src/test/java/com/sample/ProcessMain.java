aaddfc485f0bb32e548877fdc4ac5a056fbc5d9b
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2014-06-23 12:40:31 +0900

    simple mvn project

---
+++ jBPM6Ex18_mvn/src/test/java/com/sample/ProcessMain.java
@@ -0,0 +1,68 @@
+package com.sample;
+
+import java.util.List;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import junit.framework.TestCase;
+
+import org.jbpm.test.JBPMHelper;
+import org.junit.Test;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.manager.RuntimeEngine;
+import org.kie.api.runtime.manager.RuntimeEnvironmentBuilder;
+import org.kie.api.runtime.manager.RuntimeManager;
+import org.kie.api.runtime.manager.RuntimeManagerFactory;
+import org.kie.api.task.TaskService;
+import org.kie.api.task.model.TaskSummary;
+
+public class ProcessMain extends TestCase {
+
+    @Test
+	public void testProcess() {
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks.getKieClasspathContainer();
+		KieBase kbase = kContainer.getKieBase();
+
+		RuntimeManager manager = createRuntimeManager(kbase);
+		RuntimeEngine engine = manager.getRuntimeEngine(null);
+		KieSession ksession = engine.getKieSession();
+		TaskService taskService = engine.getTaskService();
+
+		ksession.startProcess("com.sample.bpmn.hello");
+
+		// let john execute Task 1
+		List<TaskSummary> list = taskService.getTasksAssignedAsPotentialOwner("john", "en-UK");
+		TaskSummary task = list.get(0);
+		System.out.println("John is executing task " + task.getName());
+		taskService.start(task.getId(), "john");
+		taskService.complete(task.getId(), "john", null);
+
+		// let mary execute Task 2
+		list = taskService.getTasksAssignedAsPotentialOwner("mary", "en-UK");
+		task = list.get(0);
+		System.out.println("Mary is executing task " + task.getName());
+		taskService.start(task.getId(), "mary");
+		taskService.complete(task.getId(), "mary", null);
+
+		manager.disposeRuntimeEngine(engine);
+		
+		assertTrue(true);
+	}
+
+	private RuntimeManager createRuntimeManager(KieBase kbase) {
+		JBPMHelper.startH2Server();
+		JBPMHelper.setupDataSource();
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("org.jbpm.persistence.jpa");
+		RuntimeEnvironmentBuilder builder = RuntimeEnvironmentBuilder.Factory.get()
+			.newDefaultBuilder().entityManagerFactory(emf)
+			.knowledgeBase(kbase);
+		return RuntimeManagerFactory.Factory.get()
+			.newSingletonRuntimeManager(builder.get(), "com.sample:jbpm-example:1.0.0-SNAPSHOT");
+	}
+
+}
